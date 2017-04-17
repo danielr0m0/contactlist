1eb2a7e5e466bd17705ad643fe2b017b1f805b65
danielr0m0 <drdanielromo@gmail.com>
2017-04-17 15:35:34 -0700

    update server

--- public/controllers/controller.js
+++ public/controllers/controller.js
@@ -2,6 +2,7 @@ var myApp = angular.module('myApp', []);
 myApp.controller('AppCtrl', ['$scope', '$http', function($scope, $http) {
     
     console.log("Hello World from controller");
+    
     var refresh= function(){
         $http.get('/contactlist').then(function(response){
 
